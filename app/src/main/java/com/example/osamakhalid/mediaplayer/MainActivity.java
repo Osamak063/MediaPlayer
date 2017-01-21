e80f359b3f6a8338e65743856075a1a97ca24cd3
Osama Khalid <Osama Khalid>
2017-01-22 01:59:48 +0500

    Initial commit

---
+++ app/src/main/java/com/example/osamakhalid/mediaplayer/MainActivity.java
@@ -0,0 +1,20 @@
+
+
++package com.example.osamakhalid.mediaplayer;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.osamakhalid.mediaplayer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
